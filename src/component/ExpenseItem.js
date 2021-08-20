21d9cc028fa9da65c4939ff65e8c8aa1ca2bd3a6
Gerardo <gerardo.medina@sw-argos.com>
2021-08-20 17:43:00 -0500

    avance 20/08/2021

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expeseTitle = 'Car Insurance';
-  const expenseAmmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expeseTitle}</h2>
-        <div className="expense-item__price">{expenseAmmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
