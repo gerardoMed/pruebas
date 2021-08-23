55b2b96e062d2746f2fd7007355e9a83913ed171
Gerardo <gerardo.medina@sw-argos.com>
2021-08-23 14:05:31 -0500

    cambios 23/08/2021

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import ExpenseItem from "./component/ExpenseItem";
+import Expenses from "./component/Expenses";
 
 function App() {
   const expenses = [
@@ -9,7 +9,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -27,21 +32,7 @@ function App() {
   return (
     <div>
       <h2>let's get started!</h2>
-      <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}
-        ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title} 
-        amount={expenses[1].amount} 
-        date={expenses[1].date}
-        ></ExpenseItem>
-      <ExpenseItem 
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-        date={expenses[2].date}
-        ></ExpenseItem>
+      <Expenses item={expenses}/>
     </div>
   );
 }
